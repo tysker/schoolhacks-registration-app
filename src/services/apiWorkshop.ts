c4eac8e3689c8b5e0fb885ef4b0bb29e12808f88
tysker <joergoertel@proton.me>
2024-03-26 11:03:53 +0100

    first git hub workflow test

--- src/services/apiWorkshop.ts
+++ src/services/apiWorkshop.ts
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
 
-let URL = "";
-if (import.meta.env.MODE === 'production') {
-    URL += 'http://server:3000/api/v1';
-} else {
-    URL += 'http://localhost:3000/api/v1';
-}
+// let URL = "";
+// if (import.meta.env.MODE === 'production') {
+//     URL += 'http://server:3000/api/v1';
+// } else {
+//     URL += 'http://localhost:3000/api/v1';
+// }
+
+const URL = 'http://localhost:3000/api/v1';
 
 console.log(import.meta.env.MODE);
 

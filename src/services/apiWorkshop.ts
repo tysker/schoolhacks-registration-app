d87f07faa881ec29023fab73ed3b1dc727b14d14
tysker <joergoertel@proton.me>
2024-03-26 14:47:06 +0100

    docker test

--- src/services/apiWorkshop.ts
+++ src/services/apiWorkshop.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 let URL = "";
 if (import.meta.env.MODE === 'production') {
-    URL += 'server:3000/api/v1';
+    URL += 'server/api/v1';
 } else {
     URL += 'http://localhost:3000/api/v1';
 }

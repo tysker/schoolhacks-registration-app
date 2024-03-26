b0af696872e6df66b2ddc73365739ed8a0b55d23
tysker <joergoertel@proton.me>
2024-03-26 11:34:38 +0100

    first git hub workflow test

--- src/services/apiWorkshop.ts
+++ src/services/apiWorkshop.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 let URL = "";
 if (import.meta.env.MODE === 'production') {
     console.log(URL)
-    URL += 'server:3000/api/v1';
+    URL += 'http://server:3000/api/v1';
 } else {
     console.log(URL)
     URL += 'http://localhost:3000/api/v1';

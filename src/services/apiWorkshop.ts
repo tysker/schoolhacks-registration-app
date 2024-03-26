4e371dcc03d02172ecdb71743ebabc128c0013e2
tysker <joergoertel@proton.me>
2024-03-26 09:44:41 +0100

    first git hub workflow test

--- src/services/apiWorkshop.ts
+++ src/services/apiWorkshop.ts
@@ -1,36 +1,46 @@
 import axios from 'axios';
 
+
+let URL = "";
+if (import.meta.env.MODE === 'production') {
+    URL += 'http://server:3000/api/v1';
+} else {
+    URL += 'http://localhost:3000/api/v1';
+}
+
+console.log(import.meta.env.MODE);
+
 export const getWorkshops = async () => {
-  const response = await axios.get<AxiosResponseDataWorkShops>(
-    'http://localhost:3000/api/v1/workshops',
-  );
-  return response.data.workshops;
+    const response = await axios.get<AxiosResponseDataWorkShops>(
+        `${URL}/workshops`,
+    );
+    return response.data.workshops;
 };
 
 export const getWorkshopFromParamID = async (id: string) => {
-  try {
-    const response = await axios.get<AxiosResponseDataWorkShop>(
-      `http://localhost:3000/api/v1/workshops/${id}`,
-    );
-    return response.data.workshop;
-  } catch (error: unknown) {
-    throw new Error('ContainerLarge not found');
-  }
+    try {
+        const response = await axios.get<AxiosResponseDataWorkShop>(
+            `${URL}/workshops/${id}`,
+        );
+        return response.data.workshop;
+    } catch (error: unknown) {
+        throw new Error('ContainerLarge not found');
+    }
 };
 
 export const signUpUserAndAddToWorkshop = async (id: string, user: User) => {
-  try {
-    user['name'] =
-      user.name.substring(0, 1).toUpperCase() +
-      user.name.substring(1).toLowerCase();
+    try {
+        user['name'] =
+            user.name.substring(0, 1).toUpperCase() +
+            user.name.substring(1).toLowerCase();
 
-    const response = await axios.post<AxiosResponseDataWorkShop>(
-      `http://localhost:3000/api/v1/users/${id}`,
-      user,
-    );
-    return response.data.workshop;
-  } catch (error) {
-    // @ts-expect-error
-    throw new Error(error.response.data.message);
-  }
+        const response = await axios.post<AxiosResponseDataWorkShop>(
+            `${URL}/users/${id}`,
+            user,
+        );
+        return response.data.workshop;
+    } catch (error) {
+        // @ts-expect-error
+        throw new Error(error.response.data.message);
+    }
 };

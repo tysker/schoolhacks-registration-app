e64110737f95ff1f1ad8ab709a3b82e25e2ec679
tysker <joergoertel@proton.me>
2024-04-15 17:10:08 +0200

    switch to router 6 createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Navigate,
-    RouterProvider,
-} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import AppLayout from './ui/AppLayout.tsx';
 import Workshop from './ui/pages/Workshop.tsx';
 import Error from './features/error/Error.tsx';
@@ -13,26 +8,47 @@ import {
     workshopsLoader,
     workshopLoader,
 } from './services/routerLoaderFunctions.ts';
-import {signUpUser} from './services/routerActionFunctions.ts';
+import { signUpUser } from './services/routerActionFunctions.ts';
 import SchoolHack from './ui/pages/SchoolHack.tsx';
-import {Route} from "react-router";
-
 
 function App() {
+    const router = createBrowserRouter([
+        {
+            element: <AppLayout />,
+            errorElement: <Error />,
+            children: [
+                {
+                    path: '/',
+                    element: <SchoolHack />,
+                },
+                {
+                    path: '/workshops',
+                    element: <WorkshopList />,
+                    loader: workshopsLoader,
+                    errorElement: <Error />,
+                },
+                {
+                    path: '/:id',
+                    element: <Workshop />,
+                    loader: ({ params }) => workshopLoader(params.id!),
+                    action: signUpUser,
+                    errorElement: <Error />,
+                },
+                {
+                    path: '/confirmation',
+                    element: <Confirmation />,
+                    loader: ({ params }) => ({
+                        name: params.name,
+                        email: params.email,
+                        workshop: params.workshop,
+                    }),
+                    errorElement: <Error />,
+                },
+            ],
+        },
+    ]);
 
-
-    const router = createBrowserRouter(createRoutesFromElements(
-            <Route errorElement={<Error/>} element={<AppLayout/>}>
-                <Route index element={<Navigate replace to="/schoolhack" />} />
-                <Route path="/schoolhack" element={<SchoolHack />} />
-                <Route path="/workshops" loader={workshopsLoader} element={<WorkshopList />} />
-                <Route path="/:id" action={signUpUser} loader={({params}) => workshopLoader(params.id!)} element={<Workshop />} />
-                <Route path="/confirmation" element={<Confirmation />} />
-            </Route>
-    ))
-
-
-    return <RouterProvider router={router}></RouterProvider>
+    return <RouterProvider router={router} />;
 }
 
 export default App;
